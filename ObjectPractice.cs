9058237e681f30d43966580bfdff71c9693bcd7e
SeedPaths-User <lessdismissive@email.com>
2014-04-25 14:01:16 -0600

    initial check in

---
+++ ObjectPractice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Practice
+{
+    class Pizza
+    {
+        public string pizzaSize { get; set; }
+        public toppings toppings { get; set; }
+        public string saucey { get; set; }
+    }
+}
+        static void Main(string[] args)
+        {
+
+        public Pizza(string saucey, string pizzaSize)
+{
+this.Saucey = saucey;
+this.pizzaSize = pizzaSize;
+this.toppings = new toppings();
+}
+         
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Practice
+{
+    class toppings
+    {
+
+        public string options { get; set; }
+        
+
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
